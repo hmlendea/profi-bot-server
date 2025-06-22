06d493e0896e3084f8e057fad41c98d7280b402f
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-06-22 14:25:21 +0300

    notifications: Retry sending SMTP notif when timing out

--- Service/Notifications/SmtpNotifier.cs
+++ Service/Notifications/SmtpNotifier.cs
@@ -7,16 +7,32 @@ namespace ProfiBotServer.Service.Notifications
     public class SmtpNotifier(SmtpNotifierSettings smtpNotifierSettings) : ISmtpNotifier
     {
         public void Send(string recipient, string subject, string body)
+            => DoSend(recipient, subject, body, 3);
+
+        void DoSend(string recipient, string subject, string body, int attemptsLeft)
         {
-            using var client = new SmtpClient(smtpNotifierSettings.Host, smtpNotifierSettings.Port)
+            try
             {
-                Credentials = new NetworkCredential(smtpNotifierSettings.Username, smtpNotifierSettings.Password),
-                EnableSsl = true
-            };
 
-            using var message = new MailMessage(smtpNotifierSettings.Username, recipient, subject, body);
+                using var client = new SmtpClient(smtpNotifierSettings.Host, smtpNotifierSettings.Port)
+                {
+                    Credentials = new NetworkCredential(smtpNotifierSettings.Username, smtpNotifierSettings.Password),
+                    EnableSsl = true
+                };
 
-            client.Send(message);
+                using var message = new MailMessage(smtpNotifierSettings.Username, recipient, subject, body);
+
+                client.Send(message);
+            }
+            catch (SmtpException ex) when (ex.Message.Contains("timed out"))
+            {
+                if (attemptsLeft <= 0)
+                {
+                    throw;
+                }
+
+                DoSend(recipient, subject, body, attemptsLeft - 1);
+            }
         }
     }
 }
